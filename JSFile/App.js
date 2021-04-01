3944183782fed929420f523ee0132b6c64dabf0a
AliShiyyab <alishiyyab1998@gmail.com>
2021-04-01 13:55:06 -0700

    Last Additions

---
+++ JSFile/App.js
@@ -0,0 +1,10 @@
+alert("Hello in CV Makers")
+function CHECKEDTANAME() {
+    var inputText = prompt("Please enter your TA name", "Here!");
+    if (inputText == 'Saja'){
+      confirm("Welcome!");
+    }
+    else{
+      confirm("you are not TA ");
+    }
+}
