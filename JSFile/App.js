0839ae6200fe9bd7d6d8861a3fffb2d62ea7e71b
AliShiyyab <alishiyyab1998@gmail.com>
2021-04-04 15:23:48 -0700

    HELLO JS

--- JSFile/App.js
+++ JSFile/App.js
@@ -1,10 +1,54 @@
-alert("Hello in CV Makers")
 function CHECKEDTANAME() {
-    var inputText = prompt("Please enter your TA name", "Here!");
-    if (inputText == 'Saja'){
-      confirm("Welcome!");
+  alert("Hello in CV Makers, if You need to login in Admin page we should be answered")
+    var makerName = 'Ali';
+    var name = prompt('Please Enter Maker name : ');
+    for (var i = 0 ; i < 10 ; i++)
+    {
+      if (name == makerName){
+      break;
+      }
+      else{
+        continue;
+      }
+    }
+    var passWord = '123456789';
+    var adminPassWord = prompt('Please Enter Maker password : ');
+    if (passWord == adminPassWord){
+      var Guestname = prompt('Enter name : ');
+      alert('Welcome Admin' + Guestname);
     }
     else{
-      confirm("you are not TA ");
+      alert('I"m only admin, You are Hacker ?')
+      document.write('<img src="https://www.pandasecurity.com/en/mediacenter/src/uploads/2019/07/pandasecurity-How-do-hackers-pick-their-targets.jpg"/>');
     }
+}
+
+function SaveData(){
+  alert("Please re-enter own data.");
+  for (var i = 0 ; i <= 4 ; i++){
+    switch(i){
+      case 0:
+        var x = prompt("Enter Full name:");
+        break;
+      
+      case 1:
+        var x = prompt("Eduacation : ");
+        break;
+      
+      case 2:
+        var x = prompt("How many Skills are you have ? ");
+        for (var z = 1 ; z <= x ; z++){
+          var skills = prompt("The " + z + " Skills is : ")  
+        }
+        break;
+      
+      case 3:
+        var x = prompt("Project: ");
+        break;
+      
+      default:
+        alert("You are finished ! ");
+      }
+  }
+
 }
