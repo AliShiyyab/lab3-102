82e41023e2a75ff9057d31d3f824f02b290a5294
AliShiyyab <alishiyyab1998@gmail.com>
2021-04-04 18:55:22 -0700

    JAVASCRIPPPPP

--- JSFile/App.js
+++ JSFile/App.js
@@ -1,4 +1,4 @@
-alert("HHHHH");
+alert("Welcome in MY Website");
 function CHECKEDTANAME() {
   alert("Hello in CV Makers, if You need to login in Admin page we should be answered");
     var makerName = 'Ali';
